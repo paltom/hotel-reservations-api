01c63264b38b67ab48bfb76ecd453275ec2d8370
paltom <tom0palka@gmail.com>
2021-07-22 15:25:40 +0000

    Create hotel application

--- hra/hra/settings.py
+++ hra/hra/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hotel.apps.HotelConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
