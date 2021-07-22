f8c178252864f5404436e0810fcc7dcf10b88b4d
paltom <tom0palka@gmail.com>
2021-07-22 16:35:09 +0000

    Create initial models

--- hra/hotel/models.py
+++ hra/hotel/models.py
@@ -1,3 +1,16 @@
+from decimal import Decimal
+from django.core.validators import DecimalValidator, MinValueValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class RoomClass(models.Model):
+    room_class = models.CharField(
+        'class of the room', primary_key=True, max_length=3, validators=[RegexValidator('[A-Z]')])
+    price = models.DecimalField("room's class' price for one day", decimal_places=2,
+                                max_digits=7, validators=[MinValueValidator(Decimal('0.00'))])
+
+
+class Room(models.Model):
+    number = models.CharField('room number', primary_key=True, max_length=5)
+    room_class = models.ForeignKey(
+        RoomClass, on_delete=models.DO_NOTHING, related_name='+')
