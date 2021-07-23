aaed559a5c5748c2a6be35b79b7e154c03719f84
paltom <tom0palka@gmail.com>
2021-07-23 07:27:08 +0000

    Update models

--- hra/hotel/models.py
+++ hra/hotel/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class RoomClass(models.Model):
     room_class = models.CharField(
-        'class of the room', primary_key=True, max_length=3, validators=[RegexValidator('[A-Z]')])
+        'class of the room', primary_key=True, max_length=1, validators=[RegexValidator('[A-Z]')])
     price = models.DecimalField("room's class' price for one day", decimal_places=2,
                                 max_digits=7, validators=[MinValueValidator(Decimal('0.00'))])
 
@@ -13,4 +13,4 @@ class RoomClass(models.Model):
 class Room(models.Model):
     number = models.CharField('room number', primary_key=True, max_length=5)
     room_class = models.ForeignKey(
-        RoomClass, on_delete=models.DO_NOTHING, related_name='+')
+        RoomClass, on_delete=models.CASCADE, related_name='rooms+')
